d5aa6669c09f2b0ecd75548147d53ce3ed225e1a
Swapnil Rane <swapnilrane24@gmail.com>
2018-12-30 13:43:33 +0530

    Added ECS and Script
    Added the ECS to project and created Rotator script to rotate game objects.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Rotator : MonoBehaviour {
+
+    public float speed;
+}
+
+public class RotateComponent : ComponentSystem
+{
+    struct Component
+    {
+        public Rotator rotator;
+        public Transform transform;
+    }
+
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.deltaTime;
+
+        foreach (var e in GetEntities<Component>())
+        {
+            e.transform.Rotate(0, e.rotator.speed * deltaTime, 0);
+        }
+    }
+
+}
